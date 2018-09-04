c7e2e2635e9309f79d7cea88541426f8bac69cbe
liudonghua123 <liudonghua123@gmail.com>
2018-09-04 14:54:52 +0800

    remove untracked files

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git pull
+
 if [ -d python-courses-show ]
 then
 	cd python-courses-show
@@ -13,6 +15,17 @@ npm i
 npm run build
 
 cd ..
+
+if [ -d public/courses/2017-autumn ]
+then
+	cd public/courses/2017-autumn 
+	git pull
+else
+	git clone https://github.com/ynu-python-course/2017-autumn public/courses/2017-autumn 
+	cd public/courses/2017-autumn 
+fi
+
+cd ../../..
 
 docker-compose build
 docker-compose up -d

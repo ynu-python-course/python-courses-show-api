fce9ccb2d79f7cb67c6b84219442d6e28c547594
liudonghua123 <liudonghua123@gmail.com>
2018-09-04 10:51:48 +0800

    initial commit

---
+++ src/api.js
@@ -0,0 +1,256 @@
+import {
+  Router
+} from 'express';
+import fs from 'fs';
+import path from 'path';
+
+const routes = Router();
+
+const courseRootDir = path.join(__dirname, '../public/courses/')
+const webRoot = path.join(__dirname, '../public/')
+
+// https://gist.github.com/kethinov/6658166
+
+/**
+ * return the data structure needed for https://github.com/phraniiac/react-ui-tree/
+ * @param {*} dir
+ * @param {*} parentNode
+ * @return a object like this
+  {
+    "module": "courses",
+    "collapsed": true,
+    "children": [{
+        "module": "Python-2018-春",
+        "collapsed": true,
+        "children": [{
+            "module": "team1",
+            "collapsed": true,
+            "children": [{
+                "module": "README.md",
+                "leaf": true
+            }, {
+                "module": "src",
+                "collapsed": true,
+                "children": [{
+                    "module": "hello.py",
+                    "leaf": true
+                }]
+            }]
+        }, {
+            "module": "team2",
+            "collapsed": true,
+            "children": [{
+                "module": "README.md",
+                "leaf": true
+            }, {
+                "module": "src",
+                "collapsed": true,
+                "children": [{
+                    "module": "hello.py",
+                    "leaf": true
+                }]
+            }]
+        }]
+    }]
+  }
+ */
+const walkDirForReactUiTree = function (dir, parentNode) {
+  parentNode = parentNode || {
+    module: path.basename(dir),
+    collapsed: true,
+    children: []
+  };
+  const files = fs.readdirSync(dir);
+  files.forEach(function (file) {
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      // if the file is directory
+      const subParentNode = {
+        module: file,
+        collapsed: true,
+        children: []
+      };
+      walkDirForReactUiTree(path.join(dir, file), subParentNode);
+      parentNode['children'].push(subParentNode);
+    } else {
+      // if the file is file
+      parentNode['children'].push({
+        module: file,
+        leaf: true
+      });
+    }
+  });
+  return parentNode;
+};
+
+/**
+ *
+ * return the data structure needed for https://github.com/frontend-collective/react-sortable-tree-theme-file-explorer
+ * @param {*} dir
+ * @param {*} parentNode
+ * @return a object like this
+  [
+    {
+        title: '.gitignore'
+    },
+    {
+        title: 'package.json'
+    },
+    {
+        title: 'src',
+        isDirectory: true,
+        expanded: true,
+        children: [{
+                title: 'styles.css'
+            },
+            {
+                title: 'index.js'
+            },
+            {
+                title: 'reducers.js'
+            },
+            {
+                title: 'actions.js'
+            },
+            {
+                title: 'utils.js'
+            },
+        ],
+    },
+    {
+        title: 'tmp',
+        isDirectory: true,
+        children: [{
+                title: '12214124-log'
+            },
+            {
+                title: 'drag-disabled-file',
+                dragDisabled: true
+            },
+        ],
+    },
+    {
+        title: 'build',
+        isDirectory: true,
+        children: [{
+            title: 'react-sortable-tree.js'
+        }],
+    },
+    {
+        title: 'public',
+        isDirectory: true,
+    },
+    {
+        title: 'node_modules',
+        isDirectory: true,
+    },
+  ]
+ */
+const walkDirForReactSortableTree = function (dir, parentNode) {
+  parentNode = parentNode || {
+    title: path.basename(dir),
+    expanded: true,
+    isDirectory: true,
+    children: []
+  };
+  const files = fs.readdirSync(dir);
+  files.forEach(function (file) {
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      // if the file is directory
+      const subParentNode = {
+        title: file,
+        expanded: true,
+        isDirectory: true,
+        children: []
+      };
+      walkDirForReactSortableTree(path.join(dir, file), subParentNode);
+      parentNode['children'].push(subParentNode);
+    } else {
+      // if the file is file
+      parentNode['children'].push({
+        title: file
+      });
+    }
+  });
+  return parentNode;
+};
+
+/**
+ *
+ * return the data structure needed for https://github.com/liudonghua123/react-nested-file-tree
+ * @param {*} dir
+ * @param {*} parentNode
+ * @return a object like this
+  {
+    _contents: [
+      {
+        name: 'file_name1',
+        path: 'file_name1'
+      }
+    ],
+    folder_name1: {
+      _contents: [
+        {
+          name: 'file_name2',
+          path: 'folder_name1/file_name2'
+        }
+      ],
+      folder_name2: {
+        _contents: [
+          {
+            name: 'file_name3',
+            path: 'folder_name1/folder_name2/file_name3'
+          }
+        ]
+      }
+    }
+  }
+ */
+const walkDirForReactNestedFileTree = function (dir, parentNode) {
+  parentNode = parentNode || {
+    _contents: []
+  };
+  const files = fs.readdirSync(dir);
+  files.forEach(function (file) {
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      // if the file is directory
+      const subParentNode = {
+        _contents: []
+      };
+      walkDirForReactNestedFileTree(path.join(dir, file), subParentNode);
+      parentNode[path.basename(path.join(dir, file))] = subParentNode;
+    } else {
+      // if the file is file
+      parentNode['_contents'].push({
+        name: file,
+        path: '/' + path.relative(webRoot, path.join(dir, file)).replace(/\\/g, '/')
+      });
+    }
+  });
+  return parentNode;
+};
+
+
+
+/**
+ * GET directory travse data for specified type
+ */
+routes.get('/', (req, res) => {
+  const {
+    type
+  } = req.query;
+  let courseTree;
+  switch (type) {
+    case 'reactUiTree':
+      courseTree = walkDirForReactUiTree(courseRootDir);
+      break;
+    case 'reactSortableTree':
+      courseTree = walkDirForReactSortableTree(courseRootDir);
+      break;
+    default:
+      courseTree = walkDirForReactNestedFileTree(courseRootDir);
+      break;
+  }
+  res.json(courseTree);
+});
+
+export default routes;
---
+++ src/app.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import routes from './routes';
+import api from './api';
+
+const app = express();
+app.disable('x-powered-by');
+
+// View engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
+
+// Enable cors
+app.use(cors({
+  exposedHeaders: "*"
+}));
+
+app.use(logger('dev', {
+  skip: () => app.get('env') === 'test'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.use('/', routes);
+app.use('/api', api);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  res
+    .status(err.status || 500)
+    .render('error', {
+      message: err.message
+    });
+    console.error(`err: ${JSON.stringify(err)}`);
+});
+
+export default app;
